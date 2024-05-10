623700fc3fdc7702e50adee8b778829c917c4a05
Daria Karpenko <dariakarpenko@air-de-daria.home>
2024-05-10 18:25:15 +0200

    error correction

--- script.js
+++ script.js
@@ -6,8 +6,42 @@ function toggleAnswer(number) {
     answer.classList.add("show");
   }
 }
-confetti({
-  particleCount: 100,
-  spread: 70,
-  origin: { y: 0.6 },
+const count = 200,
+  defaults = {
+    origin: { y: 0.7 },
+  };
+
+function fire(particleRatio, opts) {
+  confetti(
+    Object.assign({}, defaults, opts, {
+      particleCount: Math.floor(count * particleRatio),
+    })
+  );
+}
+
+fire(0.25, {
+  spread: 26,
+  startVelocity: 55,
+});
+
+fire(0.2, {
+  spread: 60,
+});
+
+fire(0.35, {
+  spread: 100,
+  decay: 0.91,
+  scalar: 0.8,
+});
+
+fire(0.1, {
+  spread: 120,
+  startVelocity: 25,
+  decay: 0.92,
+  scalar: 1.2,
+});
+
+fire(0.1, {
+  spread: 120,
+  startVelocity: 45,
 });
